cd9ed5ee696844d9e5bb873adaa89485c70a81af
Martin White <Guddler@users.noreply.github.com>
2024-10-21 00:40:26 +0100

    Optimised connection speed on reconnect

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+/*
+    The Bluetooth BLE portion of this code is heavily based on the example code from
+    v1.4.2 of the NimBLE Arduino bluetooth library: https://github.com/h2zero/NimBLE-Arduino
 
+    BLE client reconnection is borrowed from a BLE explorer type application, which
+    is itself based upon h2zero/NimBLE-Arduino: https://github.com/esp32beans/ESP32-BLE-HID-exp
+
+*/
 #include <Arduino.h>
 #include <NimBLEDevice.h>
 
@@ -13,6 +20,7 @@ static NimBLEUUID charUUID((uint16_t)0x2a4d);
 
 static uint8_t LED = 2;
 
+static NimBLEAddress LastBLEAddress;
 static uint32_t scanTime = 30;  // * 1000;
 static bool doConnect = false;
 static bool connected = false;
@@ -23,8 +31,7 @@ static NimBLEAdvertisedDevice *myDevice;
 void flashLED();
 
 /** Notification / Indication receiving handler callback */
-void notifyCallback(NimBLERemoteCharacteristic *pRemoteCharacteristic, uint8_t *pData,
-                    size_t length, bool isNotify) {
+void notifyCallback(NimBLERemoteCharacteristic *pRemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
 	Serial.print("Notify callback for characteristic: ");
 	Serial.print(pRemoteCharacteristic->getUUID().toString().c_str());
 	Serial.printf(", of data length: %d", length);
@@ -41,8 +48,7 @@ void notifyCallback(NimBLERemoteCharacteristic *pRemoteCharacteristic, uint8_t *
 	// appears to be related to the volume + / - / mute
 	if (length == 7) {
 		Serial.print("KeyCodes: ");
-		for (size_t i = 1; i < 7; i++)
-			Serial.printf("%c ", pData[0] == 2 ? shift_keys[pData[i]] : keys[pData[i]]);
+		for (size_t i = 1; i < 7; i++) Serial.printf("%c ", pData[0] == 2 ? shift_keys[pData[i]] : keys[pData[i]]);
 		Serial.println();
 
 		flashLED();
@@ -55,8 +61,7 @@ class MyClientCallback : public NimBLEClientCallbacks {
 		Serial.println(" - Device connected");
 		Serial.printf(" - Bonded device count: %d\n", NimBLEDevice::getNumBonds());
 		for (int i = 0; i < NimBLEDevice::getNumBonds(); i++) {
-			Serial.printf(" - Bonded device: %s\n",
-			              NimBLEDevice::getBondedAddress(i).toString().c_str());
+			Serial.printf(" - Bonded device: %s\n", NimBLEDevice::getBondedAddress(i).toString().c_str());
 		}
 
 		pClient->updateConnParams(120, 120, 0, 60);
@@ -67,32 +72,84 @@ class MyClientCallback : public NimBLEClientCallbacks {
 		Serial.println(" - Device disconnected");
 		Serial.printf(" - Bonded device count: %d\n", NimBLEDevice::getNumBonds());
 		for (int i = 0; i < NimBLEDevice::getNumBonds(); i++) {
-			Serial.printf(" - Bonded device: %s\n",
-			              NimBLEDevice::getBondedAddress(i).toString().c_str());
+			Serial.printf(" - Bonded device: %s\n", NimBLEDevice::getBondedAddress(i).toString().c_str());
 		}
 	}
 
-	void onAuthenticationComplete(ble_gap_conn_desc *desc) {
-		Serial.println(" - Authentication complete");
-	}
+	void onAuthenticationComplete(ble_gap_conn_desc *desc) { Serial.println(" - Authentication complete"); }
 };
 
 /** Handles the provisioning of clients and connects / interfaces with the server */
 bool connectToServer() {
+	Serial.printf("Client List Size: %d\n", NimBLEDevice::getClientListSize());
+
+	NimBLEClient *pClient = nullptr;
+	bool reconnected = false;
+
 	Serial.print("Forming a connection to ");
 	Serial.println(myDevice->getAddress().toString().c_str());
 
-	NimBLEClient *pClient = NimBLEDevice::createClient();
-	Serial.println(" - Created client");
-
-	pClient->setClientCallbacks(new MyClientCallback());
-
-	// Connect to the remote BLE Server.
-	if (!pClient->connect(myDevice)) {
-		// if you pass BLEAdvertisedDevice instead of address, it will be recognized
-		// type of peer device address (public or private)
-		return false;
+	// See if we have a client we can re-connect to. This should be much faster that doing
+	// everything again
+	if (NimBLEDevice::getClientListSize()) {
+		pClient = NimBLEDevice::getClientByPeerAddress(myDevice->getAddress());
+		if (pClient) {
+			if (pClient->getPeerAddress() == LastBLEAddress) {
+				Serial.printf("Reconnecting to existing client: %s\n", myDevice->getAddress().toString().c_str());
+				if (!pClient->connect(myDevice, false)) {
+					Serial.println(" - Reconnect failed");
+					return false;
+				}
+				Serial.println(" - Reconnected client");
+				reconnected = true;
+			}
+		} else {
+			// No client that knows this device, see if we can reuse a disconnected one
+			pClient = NimBLEDevice::getDisconnectedClient();
+			Serial.println(" - recycled disconnected client");
+		}
 	}
+
+	if (!pClient) {
+		// None of the reconnect or recycle options worked so try creating a new client
+		if (NimBLEDevice::getClientListSize() >= NIMBLE_MAX_CONNECTIONS) {
+			Serial.println("Max clients reached - no more connections available");
+			Serial.println("Reset or power cycle the adapter to try again");
+			return false;
+		}
+
+		pClient = NimBLEDevice::createClient();
+		Serial.println(" - Created client");
+
+		pClient->setClientCallbacks(new MyClientCallback());
+		/** Set initial connection parameters: These settings are 15ms interval, 0 latency, 120ms
+		 * timout. These settings are safe for 3 clients to connect reliably, can go faster if you
+		 * have less connections. Timeout should be a multiple of the interval, minimum is 100ms.
+		 *  Min interval: 12 * 1.25ms = 15, Max interval: 12 * 1.25ms = 15, 0 latency, 51 * 10ms =
+		 * 510ms timeout
+		 */
+		pClient->setConnectionParams(12, 12, 0, 51);
+		/** Set how long we are willing to wait for the connection to complete (seconds), default
+		 * is 30. */
+		pClient->setConnectTimeout(5);
+
+		// Connect to the remote BLE Server.
+		if (!pClient->connect(myDevice)) {
+			/** Created a client but failed to connect, don't need to keep it as it has no data */
+			NimBLEDevice::deleteClient(pClient);
+			Serial.println(" - Failed to connect. Client deleted.");
+			return false;
+		}
+	}
+
+	if (!pClient->isConnected()) {
+		if (!pClient->connect(myDevice)) {
+			Serial.println(" - Failed to connect.");
+			return false;
+		}
+	}
+
+	LastBLEAddress = pClient->getPeerAddress();
 	Serial.println(" - Connected to server");
 
 	// Obtain a reference to the service we are after in the remote BLE server.
@@ -144,14 +201,13 @@ class MyScanCallbacks : public NimBLEAdvertisedDeviceCallbacks {
 	 * Called for each advertising BLE server.
 	 */
 	void onResult(NimBLEAdvertisedDevice *advertisedDevice) {
-		Serial.print("BLE Advertised Device found: ");
-		Serial.println(advertisedDevice->toString().c_str());
-
-		// 	// We have found a device, let us now see if it contains the service we are
+		// We have found a device, let us now see if it contains the service we are
 		// looking for.
 		if (NimBLEDevice::isBonded(advertisedDevice->getAddress()) ||
 		    advertisedDevice->isAdvertisingService(serviceUUID)) {
-			Serial.println("HID service found - will attempt to connect");
+			Serial.print("HID Device found: ");
+			Serial.println(advertisedDevice->toString().c_str());
+			Serial.println(" - will attempt to connect");
 			NimBLEDevice::getScan()->stop();
 			myDevice = advertisedDevice;
 			doConnect = true;
@@ -194,9 +250,8 @@ void setup() {
 	 */
 	// NimBLEDevice::setSecurityAuth(false, false, true);
 	// NimBLEDevice::setSecurityAuth(true, false, true);
-	NimBLEDevice::setSecurityAuth(
-		BLE_SM_PAIR_AUTHREQ_BOND |
-		/* BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
+	NimBLEDevice::setSecurityAuth(BLE_SM_PAIR_AUTHREQ_BOND |
+	                              /* BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
 	// NimBLEDevice::setSecurityInitKey(3);
 
 	/** Optional: set the transmit power, default is 3db */
@@ -242,9 +297,7 @@ void loop() {
 		if (connectToServer()) {
 			Serial.println("We are now connected to the BLE Server.");
 		} else {
-			Serial.println(
-				"We have failed to connect to the server; there is nothin more we will "
-				"do.");
+			Serial.println("Failed to connect to the server; welcome to the end of the line club.");
 		}
 		doConnect = false;
 	}
